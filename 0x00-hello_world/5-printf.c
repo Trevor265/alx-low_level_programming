1b0aa38706ddd19111e4d1158fe58f127adc4906
Trevor Kunsida <trevorkunsida@gmail.com>
2022-09-08 19:48:57 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
-
 /**
- *
- * * main - Prints a text
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
-
+*main - Print 
+*
+*Return: Always 0 (Sharp)
+*/
 int main(void)
-
 {
-
-	printf("with proper grammar, but the outcome is a piece of 
-
-			art,\n");
-
+	printf("with a proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
